42e510d176e608407a576d032ce156917e95660d
Maido Käära <maidokaara@gmail.com>
2018-07-20 17:53:01 +0300

    Android implementation

--- android/src/main/kotlin/com/getchange/intercomflutter/IntercomFlutterPlugin.kt
+++ android/src/main/kotlin/com/getchange/intercomflutter/IntercomFlutterPlugin.kt
@@ -1,25 +1,55 @@
 package com.getchange.intercomflutter
 
+import android.app.Application
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.PluginRegistry.Registrar
+import io.intercom.android.sdk.Intercom
+import io.intercom.android.sdk.identity.Registration
 
-class IntercomFlutterPlugin : MethodCallHandler {
+class IntercomFlutterPlugin(private val application: Application) : MethodCallHandler {
     companion object {
         @JvmStatic
         fun registerWith(registrar: Registrar) {
             val channel = MethodChannel(registrar.messenger(), "intercom_flutter")
-            channel.setMethodCallHandler(IntercomFlutterPlugin())
+            channel.setMethodCallHandler(IntercomFlutterPlugin(registrar.context() as Application))
         }
     }
 
     override fun onMethodCall(call: MethodCall, result: Result) {
-        if (call.method == "getPlatformVersion") {
-            result.success("Android ${android.os.Build.VERSION.RELEASE}")
-        } else {
-            result.notImplemented()
+        when {
+            call.method == "initialize" -> {
+                val apiKey = call.argument<String>("apiKey")
+                val appId = call.argument<String>("appId")
+                Intercom.initialize(application, apiKey, appId)
+                result.success("Intercom initialized")
+            }
+            call.method == "registerIdentifiedUser" -> {
+                val userId = call.argument<String>("userId")
+                val registration = Registration.create().withUserId(userId)
+                Intercom.client().registerIdentifiedUser(registration)
+                result.success("User created")
+            }
+            call.method == "registerUnidentifiedUser" -> {
+                Intercom.client().registerUnidentifiedUser()
+                result.success("User created")
+            }
+            call.method == "logout" -> {
+                Intercom.client().logout()
+                result.success("logout")
+            }
+            call.method == "setLauncherVisibility" -> {
+                val visibility = call.argument<String>("visibility")
+                Intercom.client().setLauncherVisibility(Intercom.Visibility.valueOf(visibility))
+                result.success("Showing launcher")
+            }
+            call.method == "displayMessenger" -> {
+                Intercom.client().displayMessenger()
+                result.success("Launched")
+            }
+            else -> result.notImplemented()
         }
     }
 }
--- lib/intercom_flutter.dart
+++ lib/intercom_flutter.dart
@@ -3,11 +3,31 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class IntercomFlutter {
-  static const MethodChannel _channel =
-      const MethodChannel('intercom_flutter');
+  static const MethodChannel _channel = const MethodChannel('intercom_flutter');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  static Future<dynamic> initialize(String appId, String apiKey) {
+    return _channel
+        .invokeMethod('initialize', {'appId': appId, 'apiKey': apiKey});
+  }
+
+  static Future<dynamic> registerIdentifiedUser(String userId) {
+    return _channel.invokeMethod('registerIdentifiedUser', {'userId': userId});
+  }
+
+  static Future<dynamic> registerUnidentifiedUser() {
+    return _channel.invokeMethod('registerUnidentifiedUser');
+  }
+
+  static Future<dynamic> logout() {
+    return _channel.invokeMethod('logout');
+  }
+
+  static Future<dynamic> setLauncherVisibility(String visibility) {
+    return _channel
+        .invokeMethod('setLauncherVisibility', {'visibility': visibility});
+  }
+
+  static Future<dynamic> displayMessenger() {
+    return _channel.invokeMethod('displayMessenger');
   }
 }
