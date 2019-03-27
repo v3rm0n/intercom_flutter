294d8db15e9a87cd4bbf77e6bcc722d616e9351f
Cristian Zazo <cristian.zazo@gmail.com>
2019-03-27 13:07:01 +0100

    Fix 'registerIdentifiedUser arguments conflict'
    Also, added some trailing commas and indentation.
    
    Fixes #10

--- lib/intercom_flutter.dart
+++ lib/intercom_flutter.dart
@@ -10,12 +10,15 @@ class Intercom {
   static const MethodChannel _channel =
       const MethodChannel('maido.io/intercom');
 
-  static Future<dynamic> initialize(String appId,
-      {String androidApiKey, String iosApiKey}) {
+  static Future<dynamic> initialize(
+    String appId, {
+    String androidApiKey,
+    String iosApiKey,
+  }) {
     return _channel.invokeMethod('initialize', {
       'appId': appId,
       'androidApiKey': androidApiKey,
-      'iosApiKey': iosApiKey
+      'iosApiKey': iosApiKey,
     });
   }
 
@@ -24,22 +27,38 @@ class Intercom {
   }
 
   static Future<dynamic> registerIdentifiedUser({String userId, String email}) {
-    return _channel.invokeMethod(
-        'registerIdentifiedUser', {'userId': userId, 'email': email});
+    if (userId.isNotEmpty) {
+      assert(email.isEmpty,
+          'The parameter `email` must be null if `userId` is provided.');
+      return _channel.invokeMethod('registerIdentifiedUser', {
+        'userId': userId,
+      });
+    } else if (email.isNotEmpty) {
+      assert(userId.isEmpty,
+          'The parameter `userId` must be null if `email` is provided.');
+      return _channel.invokeMethod('registerIdentifiedUser', {
+        'email': email,
+      });
+    } else {
+      assert(userId.isNotEmpty || email.isNotEmpty,
+          'An identification method must be provided as a parameter, either `userId` or `email`.');
+      return registerUnidentifiedUser();
+    }
   }
 
   static Future<dynamic> registerUnidentifiedUser() {
     return _channel.invokeMethod('registerUnidentifiedUser');
   }
 
-  static Future<dynamic> updateUser(
-      {String email,
-      String name,
-      String phone,
-      String company,
-      String companyId,
-      String userId,
-      Map<String, dynamic> customAttributes}) {
+  static Future<dynamic> updateUser({
+    String email,
+    String name,
+    String phone,
+    String company,
+    String companyId,
+    String userId,
+    Map<String, dynamic> customAttributes,
+  }) {
     return _channel.invokeMethod('updateUser', <String, dynamic>{
       'email': email,
       'name': name,
@@ -47,7 +66,7 @@ class Intercom {
       'company': company,
       'companyId': companyId,
       'userId': userId,
-      'customAttributes': customAttributes
+      'customAttributes': customAttributes,
     });
   }
 
@@ -58,8 +77,9 @@ class Intercom {
   static Future<dynamic> setLauncherVisibility(IntercomVisibility visibility) {
     String visibilityString =
         visibility == IntercomVisibility.visible ? 'VISIBLE' : 'GONE';
-    return _channel.invokeMethod(
-        'setLauncherVisibility', {'visibility': visibilityString});
+    return _channel.invokeMethod('setLauncherVisibility', {
+      'visibility': visibilityString,
+    });
   }
 
   static Future<int> unreadConversationCount() {
@@ -70,8 +90,9 @@ class Intercom {
       IntercomVisibility visibility) {
     String visibilityString =
         visibility == IntercomVisibility.visible ? 'VISIBLE' : 'GONE';
-    return _channel.invokeMethod(
-        'setInAppMessagesVisibility', {'visibility': visibilityString});
+    return _channel.invokeMethod('setInAppMessagesVisibility', {
+      'visibility': visibilityString,
+    });
   }
 
   static Future<dynamic> displayMessenger() {
