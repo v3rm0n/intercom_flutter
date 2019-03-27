997b934a790382ca1dfaaffe823ff6b51b78ad78
Cristian Zazo <cristian.zazo@gmail.com>
2019-03-27 14:19:16 +0100

    Add tests
    Also, added some missing trailing commas, indentations and groupped similar tests

--- test/intercom_flutter_test.dart
+++ test/intercom_flutter_test.dart
@@ -14,26 +14,38 @@ void main() {
       final androidApiKey = 'android-key';
       final iosApiKey = 'ios-key';
 
-      Intercom.initialize(appId,
-          androidApiKey: androidApiKey, iosApiKey: iosApiKey);
+      Intercom.initialize(
+        appId,
+        androidApiKey: androidApiKey,
+        iosApiKey: iosApiKey,
+      );
 
       expectMethodCall('initialize', arguments: {
         'appId': appId,
         'androidApiKey': androidApiKey,
-        'iosApiKey': iosApiKey
+        'iosApiKey': iosApiKey,
       });
     });
 
-    test('registerIdentifiedUser with userId', () {
-      Intercom.registerIdentifiedUser(userId: 'test');
-      expectMethodCall('registerIdentifiedUser',
-          arguments: {'userId': 'test', 'email': null});
+    group('registerIdentifiedUser', () {
+      test('with userId', () {
+        Intercom.registerIdentifiedUser(userId: 'test');
+        expectMethodCall('registerIdentifiedUser', arguments: {
+          'userId': 'test',
+        });
+      });
+
+      test('with email', () {
+        Intercom.registerIdentifiedUser(email: 'test');
+        expectMethodCall('registerIdentifiedUser', arguments: {
+          'email': 'test',
+        });
+      });
     });
 
-    test('registerIdentifiedUser with email', () {
-      Intercom.registerIdentifiedUser(email: 'test');
-      expectMethodCall('registerIdentifiedUser',
-          arguments: {'email': 'test', 'userId': null});
+    test('registerUnidentifiedUser', () {
+      Intercom.registerUnidentifiedUser();
+      expectMethodCall('registerUnidentifiedUser');
     });
 
     test('setUserHash', () {
@@ -63,47 +75,47 @@ void main() {
       expectMethodCall('unreadConversationCount');
     });
 
-    test('setInAppMessagesVisibility - visible', () {
-      Intercom.setInAppMessagesVisibility(IntercomVisibility.visible);
-      expectMethodCall('setInAppMessagesVisibility', arguments: {
-        'visibility': 'VISIBLE',
+    group('setInAppMessagesVisibility', () {
+      test('visible', () {
+        Intercom.setInAppMessagesVisibility(IntercomVisibility.visible);
+        expectMethodCall('setInAppMessagesVisibility', arguments: {
+          'visibility': 'VISIBLE',
+        });
+      });
+
+      test('gone', () {
+        Intercom.setInAppMessagesVisibility(IntercomVisibility.gone);
+        expectMethodCall('setInAppMessagesVisibility', arguments: {
+          'visibility': 'GONE',
+        });
       });
     });
 
-    test('setInAppMessagesVisibility - gone', () {
-      Intercom.setInAppMessagesVisibility(IntercomVisibility.gone);
-      expectMethodCall('setInAppMessagesVisibility', arguments: {
-        'visibility': 'GONE',
+    group('setLauncherVisibility', () {
+      test('visible', () {
+        Intercom.setLauncherVisibility(IntercomVisibility.visible);
+        expectMethodCall('setLauncherVisibility', arguments: {
+          'visibility': 'VISIBLE',
+        });
       });
-    });
 
-    test('registerUnidentifiedUser', () {
-      Intercom.registerUnidentifiedUser();
-      expectMethodCall('registerUnidentifiedUser');
-    });
-
-    test('setLauncherVisibility - visible', () {
-      Intercom.setLauncherVisibility(IntercomVisibility.visible);
-      expectMethodCall('setLauncherVisibility', arguments: {
-        'visibility': 'VISIBLE',
-      });
-    });
-
-    test('setLauncherVisibility - gone', () {
-      Intercom.setLauncherVisibility(IntercomVisibility.gone);
-      expectMethodCall('setLauncherVisibility', arguments: {
-        'visibility': 'GONE',
+      test('gone', () {
+        Intercom.setLauncherVisibility(IntercomVisibility.gone);
+        expectMethodCall('setLauncherVisibility', arguments: {
+          'visibility': 'GONE',
+        });
       });
     });
 
     test('updateUser', () {
       Intercom.updateUser(
-          email: 'test@example.com',
-          name: 'John Doe',
-          userId: '1',
-          phone: '+37256123456',
-          company: 'Some Company LLC',
-          companyId: '2');
+        email: 'test@example.com',
+        name: 'John Doe',
+        userId: '1',
+        phone: '+37256123456',
+        company: 'Some Company LLC',
+        companyId: '2',
+      );
       expectMethodCall('updateUser', arguments: {
         'email': 'test@example.com',
         'name': 'John Doe',
@@ -111,7 +123,7 @@ void main() {
         'phone': '+37256123456',
         'company': 'Some Company LLC',
         'companyId': '2',
-        'customAttributes': null
+        'customAttributes': null,
       });
     });
   });
