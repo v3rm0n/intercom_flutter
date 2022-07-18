4165dac4d44c38427564bb88013eb1358ddc5afd
Mykhailo Mozhaiskyi <mihail.mozhaiskii@gmail.com>
2022-07-18 14:23:59 +0200

    Fixed typo

--- intercom_flutter/test/intercom_flutter_test.dart
+++ intercom_flutter/test/intercom_flutter_test.dart
@@ -178,7 +178,7 @@ void main() {
         'phone': '+37256123456',
         'company': {
           'id': 'some_company_llc',
-          'name': 'Soma Company LLC',
+          'name': 'Some Company LLC',
           'customAttributes': {'test': 'test'},
           'createdAt': null,
           'monthlySpend': null,
