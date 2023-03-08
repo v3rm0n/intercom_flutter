62d93a3952485168e1732ee8b585b350944ccd05
Deepak <er.deepakgoyal786@gmail.com>
2023-03-08 22:27:10 +0530

    Revert to FlutterActivity

--- intercom_flutter/example/android/app/src/main/kotlin/io/maido/intercomexample/MainActivity.kt
+++ intercom_flutter/example/android/app/src/main/kotlin/io/maido/intercomexample/MainActivity.kt
@@ -1,6 +1,6 @@
 package io.maido.intercomexample
 
-import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity() : FlutterFragmentActivity() {
+class MainActivity() : FlutterActivity() {
 }
