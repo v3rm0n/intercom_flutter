844a80813821b647cf139f1e31510933bc6fcf3a
Vinesh Raju <hi@thevinesh.com>
2023-09-04 16:29:59 +0100

    Add tests

--- intercom_flutter_platform_interface/test/method_channel_intercom_flutter_test.dart
+++ intercom_flutter_platform_interface/test/method_channel_intercom_flutter_test.dart
@@ -423,6 +423,14 @@ void main() {
         ],
       );
     });
+
+    test('displayTickets', () async {
+      await intercom.displayTickets();
+      expect(
+        log,
+        <Matcher>[isMethodCall('displayTickets', arguments: null)],
+      );
+    });
   });
 }
 
