db1f28157d2a07b72818dc85fc1b108737e2994f
Vinesh Raju <hi@thevinesh.com>
2023-09-07 10:05:48 +0100

    Format

--- intercom_flutter/lib/intercom_flutter.dart
+++ intercom_flutter/lib/intercom_flutter.dart
@@ -286,7 +286,7 @@ class Intercom {
   Future<void> displayConversation(String conversationId) {
     return IntercomFlutterPlatform.instance.displayConversation(conversationId);
   }
-  
+
   /// To display an activity with all your tickets.
   Future<void> displayTickets() {
     return IntercomFlutterPlatform.instance.displayTickets();
